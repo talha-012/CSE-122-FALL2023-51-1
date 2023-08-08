148a16133dbacac172c4acf50161b8d2e9b4309f
Talha Jubaer Prantor <139230616+talha-012@users.noreply.github.com>
2023-08-08 16:05:32 +0600

    Update 1.Beautiful Matrix.cpp

--- Lab-5/1.Beautiful Matrix.cpp
+++ Lab-5/1.Beautiful Matrix.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     int x ;
+    int res;
     for (int i = 1; i <= 5; ++i)
     {
         for (int j = 1; j <= 5; ++j)
@@ -13,9 +14,10 @@ int main()
             cin >> x;
             if (x == 1)
             {
-                cout << abs(i - 3) + abs(j - 3) << endl;
+                res= abs(i - 3) + abs(j - 3) ;
             }
         }
     }
+    cout<<res;
     return 0;
 }
