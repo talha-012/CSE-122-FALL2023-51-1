7c4715318cc677e66f0c8fe7570d9c9d12aa9ff8
Talha Jubaer Prantor <139230616+talha-012@users.noreply.github.com>
2023-09-26 10:31:41 +0600

    Create 1.Word Capitalization.cpp

---
+++ Lab-6/1.Word Capitalization.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    cin>>name;
+
+    char first=toupper(name[0]);
+    name[0]=first;
+    cout<<name<<endl;
+
+return 0;
+}
