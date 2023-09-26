6271bfd96e2de5f9e32550633c4c8df8e81d7d1d
Talha Jubaer Prantor <139230616+talha-012@users.noreply.github.com>
2023-09-26 10:42:35 +0600

    Create 3.Stones on the Table.cpp

---
+++ Lab-6/3.Stones on the Table.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int  main() 
+{
+
+ int n; cin>> n;
+    string s; cin>>s; 
+    int cnt=0; 
+
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] == str[i+1]){
+          cnt++;
+        }
+        
+    }
+
+    cout<< cnt << endl;
+
+}
+ 
