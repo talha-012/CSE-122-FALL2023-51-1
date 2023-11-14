187a128a33a98a39e4f4ea6b4aaacf40e400d6a0
Talha Jubaer Prantor <139230616+talha-012@users.noreply.github.com>
2023-11-14 10:09:08 +0600

    Update Lab Final.cpp

--- Lab Final.cpp
+++ Lab Final.cpp
@@ -1,38 +1,67 @@
 #include <iostream>
 using namespace std;
 
-class ManchesterUnited {
-    private:
-        int coach;
-        int player;
-    public:
-        ManchesterUnited() {
-            coach = 0;
-            player = 0;
-        }
-        void getData() {
-            cout << "Coach: " << coach << endl;
-            cout << "Player: " << player << endl;
-        }
-        ManchesterUnited operator++() {
-            coach++;
-            player++;
-            return *this;
-        }
+class ManchesterUnited
+{
+private:
+    int coach;
+    int player;
+public:
+
+    void getData(int a,int b)
+    {
+        coach =a;
+        player= b;
+    }
+    void increament()
+    {
+        coach++;
+        player++;
+
+    }
+    int display()
+    {
+        return coach+player;
+    }
 };
 
-int main() {
-    ManchesterUnited ronaldo;
-    ManchesterUnited fernandes;
-    ronaldo.getData();
-    fernandes.getData();
-    ronaldo++;
-    cout << "After incrementing Ronaldo by one:" << endl;
-    ronaldo.getData();
-    cout << "Before incrementing Ronaldo, Ronaldo is less than Fernandes: " << ((ronaldo.coach + ronaldo.player) < (fernandes.coach + fernandes.player)) << endl;
-    ronaldo++;
-    cout << "After incrementing Ronaldo by one:" << endl;
-    ronaldo.getData();
-    cout << "After incrementing Ronaldo, Ronaldo is less than Fernandes: " << ((ronaldo.coach + ronaldo.player) < (fernandes.coach + fernandes.player)) << endl;
+int main()
+{
+    ManchesterUnited ronaldo,fernandes;
+
+    ronaldo.getData(7,10);
+    fernandes.getData(5,12);
+
+    cout<<"Before increament ";
+    if(ronaldo.display()>fernandes.display())
+    {
+        cout<<"Ronaldo is greater";
+    }
+    else if(ronaldo.display()<fernandes.display())
+    {
+        cout<<"Fernandes is greater";
+    }
+    else if(ronaldo.display()==fernandes.display())
+    {
+        cout<<"Two are equal";
+    }
+    ronaldo.increament();
+
+    cout<<endl;
+    cout<<"After increament ";
+    if(ronaldo.display()>fernandes.display())
+    {
+        cout<<"Ronaldo is greater";
+    }
+    else if(ronaldo.display()<fernandes.display())
+    {
+        cout<<"Fernandes is greater";
+    }
+    else if(ronaldo.display()==fernandes.display())
+    {
+        cout<<"Two are equal";
+    }
+
+
     return 0;
 }
